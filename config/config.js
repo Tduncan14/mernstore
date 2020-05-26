83f485bff8e1c46c0a14ad96ececadc5f3d80aa1
Treek <nbatre1@yahoo.com>
2020-05-25 20:27:12 -0500

    keep moving forward

---
+++ config/config.js
@@ -0,0 +1,14 @@
+const config = {
+
+    port:process.env.PORT || 8000,
+    mongoURI:process.env.MONGO,
+    jwtSecret:"s39304394039"||process.env.JWT_SECRET
+
+
+
+
+
+}
+
+
+module.exports = config
---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const {MongoClient} = require('mongodb')
+const cors = require("cors");
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser')
+const config = require('../config/config');
+const bodyParser = require('body-parser');
+const compress = require('compression');
+
+const app = express();
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use(cookieParser())
+app.use(compress())
+app.use(helmet())
+app.use(cors())
+
+
+app.listen(config.port, (err) =>{
+
+    if(err){
+        console.log(err)
+    }
+
+    console.log(`listening on port ${config.port}`)
+
+})
