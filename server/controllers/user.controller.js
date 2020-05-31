438d8d9e6271c369a1116caaed51dfba21abbd1b
Treek <nbatre1@yahoo.com>
2020-05-31 16:54:48 -0500

    keep moving forward

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -126,19 +126,41 @@ const update =(req,res,next) => {
 
      }
 
-
-
-
-
-
-
-
-
-
-
 }
 
 const remove = (req,res,next) => {
+
+    try{
+
+        let user = req.profile
+        
+        let deletedUser = await user.remove()
+
+
+        deletedUser.hashed_password = undefined
+
+        deletedUser.salt = undefined
+
+        res.json(deletedUser)
+    }
+
+
+    catch(err){
+
+         return res.status(400).json({
+
+
+            error: errorHandler.getErrorMessage(err)
+         })
+
+
+
+
+
+
+    }
+
+
 
 }
 
