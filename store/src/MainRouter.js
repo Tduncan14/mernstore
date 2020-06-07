fd9371ba4e3f87c534b022772dd76a79b418a07f
Treek <nbatre1@yahoo.com>
2020-06-07 15:50:23 -0500

    keep moving forward

--- store/src/MainRouter.js
+++ store/src/MainRouter.js
@@ -1,27 +1,17 @@
-import { createMuiTheme } from '@material-ui/core/styles'
-import { pink } from '@material-ui/core/colors'
+import React from 'react';
+import  {Route,Switch} from 'react-router-dom'
+import Home from './Components/core/Home';
 
-const theme = createMuiTheme({
-    typography: {
-      useNextVariants: true,
-    },
-    palette: {
-      primary: {
-      light: '#5c67a3',
-      main: '#3f4771',
-      dark: '#2e355b',
-      contrastText: '#fff',
-    },
-    secondary: {
-      light: '#ff79b0',
-      main: '#ff4081',
-      dark: '#c60055',
-      contrastText: '#000',
-    },
-      openTitle: '#3f4771',
-      protectedTitle: pink['400'],
-      type: 'light'
-    }
-})
 
-export default theme
+
+const MainRouter = () => {
+
+   return( <div>
+       <Switch>
+           <Route exact path="/" component={Home} />
+       </Switch>
+   </div>)
+
+
+
+}
